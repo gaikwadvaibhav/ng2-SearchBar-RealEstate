69532aae2d59c78b89c32dab286de01d2fccc2db
rajkanase <rajkanase11@gmail.com>
2017-11-16 15:26:51 +0530

    update search bar

--- src/app/searchbox/searchbox.component.ts
+++ src/app/searchbox/searchbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
@@ -35,31 +35,68 @@ export class SearchboxComponent implements OnInit {
   currentPage= 1;
   
   property= false;
+  
+  rent = true;
+   @Input() 
+   count = 'RENT';
+   
+ 
+  @Output() 
+  countChange: EventEmitter<String>;
 
-  constructor() { }
+  constructor() {
+    this.countChange = new EventEmitter<String>();
+ }
+
+  increment() {
+    this.countChange.emit( );
+    this.countChange.emit(this.count);
+   
+    console.log(this.count);
+    
+  }
+  
+  onBuy() {
+    console.log("increament");
+    
+    // this.count++;
+    // this.countChange.emit(this.count);
+    this.countChange.emit(this.count);
+    console.log(this.count);
+    
+  }
+
 
   ngOnInit() {
   }
+ 
+  // showrent(){
+  //   this.rent++;
+  //   this.rentchange.emit(this.rent);
+  // }
 
-  
+  OnType(){
+    this.showtab = !this.showtab;
+  }
+
   onCard(){
     this.show=!this.show;
-    this.property=false;
+    // this.property=false;
   }
   onCancel(){
     this.show=false;
   }
   OnProperty() {
     this.property = !this.property
-    this.show=false;
+    // this.show=false;
   }
   OnSearch(){
     this.show=false;
-    this.property=false;
+    // this.property=false;
   }
   OnCity(){
     this.show=false;
-    this.property=false;
+    // this.property=false;
   }
 
    
